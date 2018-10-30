250e84a400f45b980dd56becea45be125b51e065
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-30 15:17:54 -0700

    Update #populate to add in non-pawn pieces in black/white

--- chess/board.rb
+++ chess/board.rb
@@ -17,8 +17,8 @@ class Board
   
   def move_piece(start_pos, end_pos)
     raise ArgumentError, "No piece at this pos!" if empty?(start_pos)
-    raise "Piece cannot move there" unless piece.moves.include?(end_pos)
     piece = self[start_pos] 
+    raise "Piece cannot move there" unless piece.moves.include?(end_pos)
     move_piece!(start_pos, end_pos)
   end
   
@@ -51,16 +51,28 @@ class Board
   
   def populate
     
-    grid.each_with_index do |row, i|
-      row.each_index do |j|
-        pos = [i,j]
-        if [0, 1, 6, 7].include?(i)
-          self[pos] = Rook.new(pos, self, :black)
-        else
-          self[pos] = NullPiece.instance
-        end
-      end
+    # grid.each_with_index do |row, i|
+    #   row.each_index do |j|
+    #     pos = [i,j]
+    #     if [0, 1, 6, 7].include?(i)
+    #       self[pos] = Knight.new(pos, self, :black)
+    #     else
+    #       self[pos] = NullPiece.instance
+    #     end
+    #   end
+    # end
+    # nil #return nil bc we don't want to return grid for user to see
+    [:black, :white].each do |color|
+      place_non_pawns(color)
     end
-    nil #return nil bc we want to return grid for user to see
   end
+  
+  def place_non_pawns(color)
+    pieces = [Rook, Knight, Bishop, King, Queen, Bishop, Knight, Rook]
+    i = color == :white ? 7 : 0
+    pieces.each_with_index do |piece, j|
+      place_pos = [i,j]
+      self[place_pos] = piece.new(place_pos, self, color)
+    end
+  end 
 end
