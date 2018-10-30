e5e4db33cbb85c7af932fe3c7274e391d1b1160c
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-30 15:46:37 -0700

    Add #steps method to Pawn class

--- chess/pieces/pawn.rb
+++ chess/pieces/pawn.rb
@@ -13,4 +13,23 @@ class Pawn < Piece
   def forward_move
     color == :white ? -1 : 1
   end
+  
+  def at_start?
+    pos[0] == (color == :white ? 6 : 1)
+  end
+  
+  def steps
+    row, col = pos
+    forward_one = [row + forward_move, col]
+    return [] unless board.valid_pos?(forward_one) && board.empty?(forward_one)
+    all_steps = [forward_one]
+    forward_two = [row + 2 * forward_move, col]
+    all_steps << forward_two if at_start? && board.empty?(forward_two) && 
+                                board.valid_pos?(forward_two)
+    all_steps
+  end
+  
+  def attacks
+    
+  end
 end
