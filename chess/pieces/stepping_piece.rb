c09c0bc7b94486290a67ddcb50d0b4bbac4ae59c
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-10-30 11:56:53 -0700

    Create SteppingPiece module

--- chess/pieces/stepping_piece.rb
+++ chess/pieces/stepping_piece.rb
@@ -1,3 +1,23 @@
 module SteppingPiece
   
+  def moves
+    possible_moves = []
+    move_dirs.each do |move|
+      x,y = pos
+      dx, dy = move
+      new_pos = [x + dx, y + dy]
+      
+      next unless board.valid_pos?(new_pos)
+      if board.empty?(new_pos)
+        valid_moves << new_pos
+      elsif board[new_pos].color != color
+        valid_moves << new_pos
+      end
+    end
+    possible_moves
+  end
+
+  def move_dirs
+    raise NotImplementedError
+  end
 end
